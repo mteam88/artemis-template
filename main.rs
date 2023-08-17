fe3aa1cbee0bd93b0cd3e3bc4159ec154cd5eb3f
Matthew <matthew8edelen@gmail.com>
2023-08-16 20:31:52 -0400

    initial commit

---
+++ main.rs
@@ -0,0 +1,7 @@
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // See [here](https://github.com/paradigmxyz/artemis/blob/88fb49346eb32b983fab9c7a035436ffc603a8d7/bin/artemis/src/main.rs) and [here.](https://github.com/paradigmxyz/artemis/blob/88fb49346eb32b983fab9c7a035436ffc603a8d7/examples/mev-share-arb/src/main.rs)
+    Ok(())
+}
